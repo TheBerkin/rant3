9fc9eee721c1f6f379cd20a9022dcfbefc6b1542
cpancake <anrogers529@gmail.com>
2015-02-06 21:05:13 -0500

    Added test for issue #39

---
+++ Rant.Tests/Replacers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Rant.Tests
+{
+    [TestFixture]
+    public class Replacers
+    {
+        private readonly RantEngine rant = new RantEngine();
+
+        [Test]
+        public void ReplacerOverflow()
+        {
+            rant.Do("[//x//:[char:x;64];y]");
+            Assert.Pass();
+        }
+    }
+}
