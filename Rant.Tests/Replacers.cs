0657cf42ccdb440c1c0aa9f93c8cddfb86717150
cpancake <anrogers529@gmail.com>
2015-02-08 20:53:07 -0500

    Added new tests for #39.

--- Rant.Tests/Replacers.cs
+++ Rant.Tests/Replacers.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using System.Text.RegularExpressions;
 
 namespace Rant.Tests
 {
@@ -15,8 +16,29 @@ namespace Rant.Tests
         [Test]
         public void ReplacerOverflow()
         {
-            rant.Do("[//x//:[char:x;64];y]");
-            Assert.Pass();
+            string result = rant.Do("[//x//:[char:x;64];y]");
+            Assert.AreEqual("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy", result);
+        }
+
+        [Test]
+        public void VowelReplacer()
+        {
+            string result = rant.Do("[//[aeiou]//i: The quick brown fox jumps over the lazy dog.; {a|e|i|o|u}]");
+            Assert.True(new Regex(@"Th[aeiou] q[aeiou][aeiou]ck br[aeiou]wn f[aeiou]x j[aeiou]mps [aeiou]v[aeiou]r th[aeiou] l[aeiou]zy d[aeiou]g\.").IsMatch(result));
+        }
+
+        [Test]
+        public void GroupTest()
+        {
+            string result = rant.Do(@"[//(?<a>(s|\b)(?<b>s)|t(?<b>h)|(?<b>s\b))//i:The slimy snake silently slithers through the leaves.;[group:a][rep:2]{[group:b]}]");
+            Assert.AreEqual("Thhhe ssslimy sssnake sssilently ssslithhhersss thhhrough thhhe leavesss.", result);
+        }
+
+        [Test]
+        public void MatchTest()
+        {
+            string result = rant.Do(@"[//(?<=\b(\w\w)*)\w(?=\w*)//:Isn't it a lovely afternoon?;[caps:first][match]]");
+            Assert.AreEqual("IsN'T It A LoVeLy AfTeRnOoN?", result);
         }
     }
 }
