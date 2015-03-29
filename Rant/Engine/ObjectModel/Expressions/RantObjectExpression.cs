b34c542f67ba851fab12fb12aa51858c9abebe03
TheBerkin <grenzatuna@gmail.com>
2015-03-29 16:38:14 -0500

    Add RantObjectExpression

---
+++ Rant/Engine/ObjectModel/Expressions/RantObjectExpression.cs
@@ -0,0 +1,24 @@
+﻿using Rant.Engine.Compiler;
+using Rant.Stringes;
+
+namespace Rant.Engine.ObjectModel.Expressions
+{
+    internal class RantObjectExpression : RantExpression
+    {
+        private readonly Token<R> _token;
+        private readonly RantObject _object;
+
+        public RantObjectExpression(RantObject rantObj, Token<R> token)
+        {
+            _token = token;
+            _object = rantObj;
+        }
+
+        public override RantObject Evaluate(VM vm)
+        {
+            if (_object == null)
+                throw new RantException(vm.CurrentState.Reader.Source, _token, "What did you DO??!");
+            return _object;
+        }
+    }
+}
