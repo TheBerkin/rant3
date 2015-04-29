e6e730ea9c45aaf1732d6a20f00954868a04f76a
cpancake <anrogers529@gmail.com>
2015-04-28 22:09:37 -0400

    New DicLexer, loads huge dictionaries much faster.

--- Rant/Vocabulary/DicLexer.cs
+++ Rant/Vocabulary/DicLexer.cs
@@ -1,49 +1,101 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Text.RegularExpressions;
 
 using Rant.Stringes;
 
 namespace Rant.Vocabulary
 {
-    internal static class DicLexer
-    {
-        private const RegexOptions DicRegexOptions = RegexOptions.Compiled;
+	internal static class DicLexer
+	{
+		public static IEnumerable<Token<DicTokenType>> Tokenize(string data)
+		{
+			var reader = new StringReader(data);
 
-        private static readonly Lexer<DicTokenType> Rules;
+			while (true)
+			{
+				int nextChar = reader.Read();
+				if (nextChar == -1)
+				{
+					yield return new Token<DicTokenType>(DicTokenType.EOF, "");
+					yield break;
+				}
 
-        static DicLexer()
-        {
-            Rules = new Lexer<DicTokenType>
-            {
-                {new Regex(@"\#\s*(?<value>.*?)[\s\r]*(?=\#|\||\>{1,2}|\@|$)", DicRegexOptions), DicTokenType.Directive, 2},
-                {new Regex(@"\|\s*(?<value>.*?)[\s\r]*(?=\#|\||\>{1,2}|\@|$)", DicRegexOptions), DicTokenType.Property, 2},
-                {new Regex(@"\>\s*(?<value>.*?)[\s\r]*(?=\#|\||\>{1,2}|\@|$)", DicRegexOptions), DicTokenType.Entry, 2},
-                {new Regex(@">>\s*(?<value>.*?)[\s\r]*(?=\#|\||\>{1,2}|\@|$)", DicRegexOptions), DicTokenType.DiffEntry, 2},
-                {new Regex(@"\@.*?$", DicRegexOptions | RegexOptions.Multiline), DicTokenType.Ignore, 2},
-                {new Regex(@"\s+"), DicTokenType.Ignore}
-            };
-            Rules.AddEndToken(DicTokenType.EOF);
-            Rules.Ignore(DicTokenType.Ignore);
-        }
+				char currentChar = (char)nextChar;
+				if (char.IsWhiteSpace(currentChar))
+					continue;
 
-        public static IEnumerable<Token<DicTokenType>> Tokenize(string data)
-        {
-            Token<DicTokenType> token;
-            var reader = new StringeReader(data);
-            while ((token = reader.ReadToken(Rules)).ID != DicTokenType.EOF)
-            {
-                yield return token;
-            }
-        }
-    }
+				switch (currentChar)
+				{
+					// directive
+					case '#':
+						{
+							string value = ReadRestOfLine(reader);
+							yield return new Token<DicTokenType>(DicTokenType.Directive, value.Trim());
+						}
+						break;
+					// entry or diffentry
+					case '>':
+						{
+							int peekedChar = reader.Peek();
+							if (peekedChar == -1)
+								yield break;
 
-    internal enum DicTokenType
-    {
-        Directive,
-        Entry,
-        DiffEntry,
-        Property,
-        Ignore,
-        EOF
-    }
+							bool diffmark = false;
+							// it's a diffentry
+							if ((char)peekedChar == '>')
+							{
+								diffmark = true;
+								reader.Read();
+							}
+
+							string value = ReadRestOfLine(reader, diffmark);
+							yield return new Token<DicTokenType>(diffmark ? DicTokenType.DiffEntry : DicTokenType.Entry, value.Trim());
+						}
+						break;
+					// property
+					case '|':
+						{
+							string value = ReadRestOfLine(reader);
+							yield return new Token<DicTokenType>(DicTokenType.Property, value.Trim());
+						}
+						break;
+					default:
+						throw new System.Exception("Unexpected character in dictionary.");
+				}
+			}
+		}
+
+		private static string ReadRestOfLine(StringReader reader, bool isDiffmark = false)
+		{
+			int nextChar;
+			char currentChar;
+			string value = "";
+
+			// read to the end of the line
+			while (true)
+			{
+				nextChar = reader.Read();
+				if (nextChar == -1)
+					break;
+				currentChar = (char)nextChar;
+				// we've made it to the end
+				if ((!isDiffmark && currentChar == '|') || currentChar == '>' || currentChar == '\n' || currentChar == '#')
+					break;
+				value = string.Concat(value, currentChar);
+			}
+
+			return value;
+		}
+	}
+
+	internal enum DicTokenType
+	{
+		Directive,
+		Entry,
+		DiffEntry,
+		Property,
+		Ignore,
+		EOF
+	}
 }
