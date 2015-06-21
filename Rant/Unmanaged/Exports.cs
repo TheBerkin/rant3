b0ef38d4166b50338241610996503153e5b3c004
Nicholas Fleck <nicholasfleck@gmail.com>
2015-06-21 14:17:21 -0500

    Properly inhibit GC on unmanaged instances

--- Rant/Unmanaged/Exports.cs
+++ Rant/Unmanaged/Exports.cs
@@ -28,59 +28,55 @@ namespace Rant.Unmanaged
         public static ErrorCode LoadEngine(UnmanagedRantContext context, string dictionaryPath) => context.Run(() => context.Rant = new RantEngine(dictionaryPath));
 
         [DllExport("RantCompilePatternString", CallingConvention.Cdecl)]
-        public static ErrorCode CompilePatternString(UnmanagedRantContext context, string patternString, out RantPattern patternCompiled)
+        public static ErrorCode CompilePatternString(UnmanagedRantContext context, string patternString, out Unmanaged<RantPattern> patternCompiled)
         {
             RantPattern pattern = null;
             context.Run(() => pattern = RantPattern.FromString(patternString));
-            patternCompiled = pattern;
-            GC.KeepAlive(patternCompiled);
+            patternCompiled = Unmanaged.Make(pattern);
             return context.LastErrorCode;
         }
 
         [DllExport("RantCompilePatternFile", CallingConvention.Cdecl)]
-        public static ErrorCode CompilePatternFile(UnmanagedRantContext context, string patternPath, out RantPattern patternCompiled)
+        public static ErrorCode CompilePatternFile(UnmanagedRantContext context, string patternPath, out Unmanaged<RantPattern> patternCompiled)
         {
             RantPattern pattern = null;
             context.Run(() => pattern = RantPattern.FromFile(patternPath));
-            patternCompiled = pattern;
-            GC.KeepAlive(patternCompiled);
+            patternCompiled = Unmanaged.Make(pattern);
             return context.LastErrorCode;
         }
 
         [DllExport("RantRunPattern", CallingConvention.Cdecl)]
-        public static ErrorCode RunPattern(UnmanagedRantContext context, RantPattern pattern, PatternOptions options, out RantOutput output)
+        public static ErrorCode RunPattern(UnmanagedRantContext context, Unmanaged<RantPattern> pattern, PatternOptions options, out Unmanaged<RantOutput> output)
         {
             RantOutput o = null;
-            context.Run(() => o = context.Rant.Do(pattern, options.CharLimit, options.Timeout));
-            output = o;
-            GC.KeepAlive(output);
+            context.Run(() => o = context.Rant.Do(pattern.Value, options.CharLimit, options.Timeout));
+            output = Unmanaged.Make(o);
             return context.LastErrorCode;
         }
 
         [DllExport("RantRunPatternSeed", CallingConvention.Cdecl)]
-        public static ErrorCode RunPatternSeed(UnmanagedRantContext context, RantPattern pattern, PatternOptions options, long seed, out RantOutput output)
+        public static ErrorCode RunPatternSeed(UnmanagedRantContext context, Unmanaged<RantPattern> pattern, PatternOptions options, long seed, out Unmanaged<RantOutput> output)
         {
             RantOutput o = null;
-            context.Run(() => o = context.Rant.Do(pattern, seed, options.CharLimit, options.Timeout));
-            output = o;
-            GC.KeepAlive(output);
+            context.Run(() => o = context.Rant.Do(pattern.Value, seed, options.CharLimit, options.Timeout));
+            output = Unmanaged.Make(o);
             return context.LastErrorCode;
         }
 
         [DllExport("RantGetMainValue", CallingConvention.Cdecl)]
-        public static string GetMainValue(RantOutput output) => output?.Main;
+        public static string GetMainValue(Unmanaged<RantOutput> output) => output?.Value?.Main;
 
         [DllExport("RantGetOutputChannelNames", CallingConvention.Cdecl)]
-        public static string[] GetOutputChannelNames(RantOutput output, out int count)
+        public static string[] GetOutputChannelNames(Unmanaged<RantOutput> output, out int count)
         {
-            var names = output.Select(e => e.Name).ToArray();
+            var names = output.Value.Select(e => e.Name).ToArray();
             count = names.Length;
             GC.KeepAlive(names);
             return names;
         }
 
         [DllExport("RantGetOutputValue", CallingConvention.Cdecl)]
-        public static string GetOutputValue(RantOutput output, string channelName) => output?[channelName];
+        public static string GetOutputValue(Unmanaged<RantOutput> output, string channelName) => output?.Value?[channelName];
 
         [DllExport("RantLoadPackage", CallingConvention.Cdecl)]
         public static ErrorCode LoadPackage(UnmanagedRantContext context, string packagePath) => context.Run(() => context.Rant.LoadPackage(packagePath));
---
+++ Rant/Unmanaged/Unmanaged.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Rant.Unmanaged
+{
+    internal static class Unmanaged
+    {
+        public static Unmanaged<T> Make<T>(T value) => new Unmanaged<T>(value);
+    }
+
+    internal sealed class Unmanaged<T> : IDisposable
+    {
+        private GCHandle _handle;
+        private bool _disposed;
+
+        public T Value { get; }
+
+        public Unmanaged(T value)
+        {
+            Value = value;
+            _handle = GCHandle.Alloc(this);
+        } 
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+            _handle.Free();
+            _disposed = true;
+        }
+    }
+}
