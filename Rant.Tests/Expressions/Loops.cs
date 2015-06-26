5e1abe401fc19e85b4cdd0bc37837486ccf19db1
Nicholas Fleck <nicholasfleck@gmail.com>
2015-06-25 23:41:13 -0500

    You gotta see this one, Andrew

--- Rant.Tests/Expressions/Loops.cs
+++ Rant.Tests/Expressions/Loops.cs
@@ -28,5 +28,33 @@ namespace Rant.Tests.Expressions
 ]");
             Assert.AreEqual("this is a test", output.Main);
         }
+
+        [Test]
+        public void Fibonacci()
+        {
+            var output = rant.Do(@"
+[@
+    {
+        var fibonacci = (n) =>
+        {
+            var a = 0;
+            var b = 1;
+            var i = 0;
+            while(i < n)
+            {
+                if (i > 0) Output.print("", "");
+                Output.print(a);
+                var temp = a;
+                a = b;
+                b += temp;
+                i++;
+            }
+        }
+        fibonacci(10);
+    }
+]
+");
+            Assert.AreEqual("0, 1, 1, 2, 3, 5, 8, 13, 21, 34", output.Main);
+        }
     }
 }
