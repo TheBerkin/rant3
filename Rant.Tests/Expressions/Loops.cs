6e0db31a528fae2fbbf953a6dab7f7e1bd766428
Nicholas Fleck <nicholasfleck@gmail.com>
2015-06-25 22:02:22 -0500

    Put loop test's variables in a local scope

--- Rant.Tests/Expressions/Loops.cs
+++ Rant.Tests/Expressions/Loops.cs
@@ -12,16 +12,19 @@ namespace Rant.Tests.Expressions
         {
             var output = rant.Do(@"
 [@
-    var parts = (""this"", ""is"", ""a"", ""test"");
-    var i = 0;
-    var buffer = """";
-    while(i < parts.length)
+    (function() 
     {
-        if (i > 0) buffer ~= "" "";
-        buffer ~= parts[i];
-        i++;
-    }
-    return buffer;
+        var parts = (""this"", ""is"", ""a"", ""test"");
+        var i = 0;
+        var buffer = """";
+        while(i < parts.length)
+        {
+            if (i > 0) buffer ~= "" "";
+            buffer ~= parts[i];
+            i++;
+        }
+        return buffer;
+    })();
 ]");
             Assert.AreEqual("this is a test", output.Main);
         }
