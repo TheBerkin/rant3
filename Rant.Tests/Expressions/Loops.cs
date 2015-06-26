2a6657b42432e6907a6705f8dea3a8f47f39eb4c
Nicholas Fleck <nicholasfleck@gmail.com>
2015-06-25 22:00:27 -0500

    Add a loop test

---
+++ Rant.Tests/Expressions/Loops.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Rant.Tests.Expressions
+{
+    [TestFixture]
+    public class Loops
+    {
+        private readonly RantEngine rant = new RantEngine();
+
+        [Test]
+        public void StringBuildingWhileLoop()
+        {
+            var output = rant.Do(@"
+[@
+    var parts = (""this"", ""is"", ""a"", ""test"");
+    var i = 0;
+    var buffer = """";
+    while(i < parts.length)
+    {
+        if (i > 0) buffer ~= "" "";
+        buffer ~= parts[i];
+        i++;
+    }
+    return buffer;
+]");
+            Assert.AreEqual("this is a test", output.Main);
+        }
+    }
+}
