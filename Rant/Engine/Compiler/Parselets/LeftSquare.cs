eaac83458aa4b19cd6e2c22e36042844a99b5df0
Spanfile <santeri.kasvi@gmail.com>
2015-11-03 18:59:48 +0200

    Regex functions should be alright

--- Rant/Engine/Compiler/Parselets/LeftSquare.cs
+++ Rant/Engine/Compiler/Parselets/LeftSquare.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 using Rant.Engine.Syntax;
@@ -38,6 +39,7 @@ namespace Rant.Engine.Compiler.Parselets
                     break;
 
                 case R.Regex:
+                    
                     break;
 
                 case R.At:
@@ -46,6 +48,25 @@ namespace Rant.Engine.Compiler.Parselets
                 case R.Dollar:
                     break;
             }
+        }
+
+        // TODO: all these "mini-parselets" are bad and DRY
+        IEnumerable<Parselet> Regex(NewRantCompiler compiler, TokenReader reader, Token<R> token)
+        {
+            Regex regex = null;
+            try
+            {
+                regex = Util.ParseRegex(token.Value);
+            }
+            catch (Exception e)
+            {
+                compiler.SyntaxError(token, e);
+            }
+
+            reader.ReadLoose(R.Colon);
+
+            foreach (var parselet in ReplacerArgs(compiler, reader, token, regex))
+                yield return parselet;
         }
 
         IEnumerable<Parselet> Text(NewRantCompiler compiler, TokenReader reader, Token<R> token)
@@ -68,13 +89,49 @@ namespace Rant.Engine.Compiler.Parselets
             }
         }
 
+        // TODO: all these "mini-compilers" are bad and DRY
+        IEnumerable<Parselet> ReplacerArgs(NewRantCompiler compiler, TokenReader reader, Token<R> token, Regex regex)
+        {
+            Token<R> funcToken = null;
+            var actions = new List<RantAction>();
+            var sequences = new List<RantAction>();
+
+            while (!reader.End)
+            {
+                funcToken = reader.ReadToken();
+
+                if (funcToken.ID == R.Semicolon)
+                {
+                    // add action to args and continue
+                    sequences.Add(actions.Count == 1 ? actions[0] : new RASequence(actions, funcToken));
+                    actions.Clear();
+                    reader.SkipSpace();
+                    continue;
+                }
+                else if (funcToken.ID == R.RightSquare)
+                {
+                    // add action to args and return
+                    sequences.Add(actions.Count == 1 ? actions[0] : new RASequence(actions, funcToken));
+
+                    if (sequences.Count != 2)
+                        compiler.SyntaxError(Stringe.Between(token, Token), "Replacer must have two arguments");
+
+                    AddToOutput(new RAReplacer(Stringe.Range(Token, token), regex, sequences[0], sequences[1]));
+                    yield break;
+                }
+
+                yield return Parselet.GetWithToken(compiler, funcToken, actions.Add);
+            }
+
+            compiler.SyntaxError(Token, "Unterminated function: unexpected end of file");
+        }
+
         IEnumerable<Parselet> FuncArgs(NewRantCompiler compiler, TokenReader reader, Token<R> token, RantFunctionGroup group)
         { 
             Token<R> funcToken = null;
             var actions = new List<RantAction>();
             var sequences = new List<RantAction>();
 
-            // TODO: all these "mini-compilers" are kinda bad
             while (!reader.End)
             {
                 funcToken = reader.ReadToken();
