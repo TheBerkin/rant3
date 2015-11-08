6b41c1252fbac3672a686f0b38a4533e01ae7d2a
Nicholas Fleck <nicholasfleck@gmail.com>
2015-11-07 22:06:50 -0600

    Add support for pattern arguments
    Closes #90

---
+++ Rant.Tests/PatternArgs.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace Rant.Tests
+{
+	[TestFixture]
+	public class PatternArgs
+	{
+		private readonly RantEngine rant = new RantEngine();
+
+		private class TestCustomArgsClass
+		{
+			[RantArg("word-a")]
+			public string A { get; set; }
+
+			[RantArg("word-b")]
+			public string B { get; set; }
+		}
+
+		[Test]
+		public void OneArg()
+		{
+			var args = new RantPatternArgs
+			{
+				["foo"] = "Bar"
+			};
+			Assert.AreEqual("FooBar", rant.Do(@"Foo[in:foo]", 0, 0, args).Main);
+		}
+
+		[Test]
+		public void OneArgAnonymousType()
+		{
+			var args = new
+			{
+				msg = "Hello World"
+			};
+
+			Assert.AreEqual("Hello World", rant.Do(@"[in:msg]", 0, 0, RantPatternArgs.CreateFrom(args)).Main);
+		}
+
+		[Test]
+		public void CustomArgsClass()
+		{
+			var args = new TestCustomArgsClass
+			{
+				A = "Hello",
+				B = "World"
+			};
+
+			Assert.AreEqual("Hello World!", rant.Do(@"[in:word-a] [in:word-b]!", 0, 0, RantPatternArgs.CreateFrom(args)).Main);
+		}
+	}
+}
---
+++ Rant/RantArgAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rant
+{
+	public sealed class RantArgAttribute : Attribute
+	{
+		public string Name { get; }
+
+		public RantArgAttribute(string name)
+		{
+			if (name == null) throw new ArgumentNullException(nameof(name));
+			Name = name;
+		}
+	}
+}
