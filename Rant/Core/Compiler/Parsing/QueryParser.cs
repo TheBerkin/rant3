1f7c192344182779f0c02c87b5b3c3a308add58e
Nicholas Fleck <nicholasfleck@gmail.com>
2016-07-25 02:43:49 -0500

    Add QueryParser skeleton class

---
+++ Rant/Core/Compiler/Parsing/QueryParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Rant.Core.Compiler.Syntax;
+
+namespace Rant.Core.Compiler.Parsing
+{
+	internal class QueryParser : Parser
+	{
+		public override IEnumerator<Parser> Parse(RantCompiler compiler, CompileContext context, TokenReader reader, Action<RantAction> actionCallback)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
