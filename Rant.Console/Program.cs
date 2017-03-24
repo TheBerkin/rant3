58e1282af3698b7d2b32ab617d430c7fea563716
Nicholas Fleck <nicholasfleck@gmail.com>
2017-03-24 14:46:06 -0500

    Modularize Rant cmdline tools

--- Rant.Console/Program.cs
+++ Rant.Console/Program.cs
@@ -15,51 +15,51 @@ using Rant.Formats;
 
 namespace RantConsole
 {
-	internal class Program
-	{
+    internal class Program
+    {
 #if !DEBUG
-		public const double PATTERN_TIMEOUT = 10.0;
+        public const double PATTERN_TIMEOUT = 10.0;
 #else
 		public const double PATTERN_TIMEOUT = 0.0;
 #endif
-		public static readonly string FILE = GetPaths().FirstOrDefault();
-		public static readonly string PKG_DIR = Property("pkgdir");
-		public static readonly string LEGACY_DIC_PATH = Property("ldict");
-		public static readonly long SEED;
-		public static readonly bool USE_SEED;
+        public static readonly string FILE = GetPaths().FirstOrDefault();
+        public static readonly string PKG_DIR = Property("pkgdir");
+        public static readonly string LEGACY_DIC_PATH = Property("ldict");
+        public static readonly long SEED;
+        public static readonly bool USE_SEED;
 
-		static Program()
-		{
-			USE_SEED = long.TryParse(Property("seed"), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out SEED);
-		}
+        static Program()
+        {
+            USE_SEED = long.TryParse(Property("seed"), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out SEED);
+        }
 
-		private static void Main(string[] args)
-		{
-			OutputEncoding = Encoding.Unicode;
-			Title = "Rant Console" + (Flag("nsfw") ? " [NSFW]" : "");
+        private static void Main(string[] args)
+        {
+            OutputEncoding = Encoding.Unicode;
+            Title = "Rant Console" + (Flag("nsfw") ? " [NSFW]" : "");
 
-			var rant = new RantEngine();
+            var rant = new RantEngine();
 
 #if !DEBUG
-			try
-			{
+            try
+            {
 #endif
-				if(!string.IsNullOrEmpty(LEGACY_DIC_PATH))
-				{
-					var tables =
-						Directory
-							.GetFiles(LEGACY_DIC_PATH)
-							.Where(f => Path.GetExtension(f) == ".dic")
-							.Select(f => RantDictionaryTable.FromLegacyFile(f));
-					rant.Dictionary = new RantDictionary(tables);
-				}
+                if (!string.IsNullOrEmpty(LEGACY_DIC_PATH))
+                {
+                    var tables =
+                        Directory
+                            .GetFiles(LEGACY_DIC_PATH)
+                            .Where(f => Path.GetExtension(f) == ".dic")
+                            .Select(f => RantDictionaryTable.FromLegacyFile(f));
+                    rant.Dictionary = new RantDictionary(tables);
+                }
 
-				if (!string.IsNullOrEmpty(PKG_DIR))
-				{
+                if (!string.IsNullOrEmpty(PKG_DIR))
+                {
 #if DEBUG
                     Stopwatch timer = Stopwatch.StartNew();
 #endif
-                    foreach(var pkg in Directory.GetFiles(PKG_DIR, "*.rantpkg"))
+                    foreach (var pkg in Directory.GetFiles(PKG_DIR, "*.rantpkg"))
                     {
                         rant.LoadPackage(pkg);
                     }
@@ -67,182 +67,186 @@ namespace RantConsole
                     timer.Stop();
                     WriteLine($"Package loading: {timer.ElapsedMilliseconds}ms");
 #endif
-				}
-				else
-				{
-					foreach (string pkg in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.rantpkg", SearchOption.AllDirectories))
-					{
-						rant.LoadPackage(pkg);
-					}
-				}
+                }
+                else
+                {
+                    foreach (string pkg in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.rantpkg", SearchOption.AllDirectories))
+                    {
+                        rant.LoadPackage(pkg);
+                    }
+                }
 #if !DEBUG
-			}
-			catch (Exception e)
-			{
-				ForegroundColor = ConsoleColor.Cyan;
-				WriteLine($"Dictionary load error: {e.Message}");
-			}
+            }
+            catch (Exception e)
+            {
+                ForegroundColor = ConsoleColor.Cyan;
+                WriteLine($"Dictionary load error: {e.Message}");
+            }
 #endif
-			if (Flag("nsfw")) rant.Dictionary.IncludeHiddenClass("nsfw");
+            if (Flag("nsfw")) rant.Dictionary.IncludeHiddenClass("nsfw");
 
-			if (!string.IsNullOrEmpty(FILE))
-			{
+            if (!string.IsNullOrEmpty(FILE))
+            {
 #if !DEBUG
-				try
-				{
+                try
+                {
 #endif
-					PrintOutput(rant, File.ReadAllText(FILE));
+                    PrintOutput(rant, FILE, true);
 #if !DEBUG
-				}
-				catch (Exception ex)
-				{
-					ForegroundColor = ConsoleColor.Red;
-					WriteLine(ex.Message);
-					ResetColor();
-				}
+                }
+                catch (Exception ex)
+                {
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine(ex.Message);
+                    ResetColor();
+                }
 #endif
 
-				if (Flag("wait")) ReadKey(true);
-				return;
-			}
+                if (Flag("wait")) ReadKey(true);
+                return;
+            }
 
-			while (true)
-			{
-				ForegroundColor = Flag("nsfw") ? ConsoleColor.DarkRed : ConsoleColor.Gray;
-				Write("RANT> ");
-				ForegroundColor = ConsoleColor.White;
-				string input = ReadLine();
-				if (input == null)
-				{
-					return;
-				}
-				PrintOutput(rant, input);
-			}
-		}
+            while (true)
+            {
+                ForegroundColor = Flag("nsfw") ? ConsoleColor.DarkRed : ConsoleColor.Gray;
+                Write("RANT> ");
+                ForegroundColor = ConsoleColor.White;
+                string input = ReadLine();
+                if (input == null)
+                {
+                    return;
+                }
+                PrintOutput(rant, input);
+            }
+        }
 
-		private static void PrintOutput(RantEngine engine, string source, bool isFile = false)
-		{
-			try
-			{
-				var sw = new Stopwatch();
+        private static void PrintOutput(RantEngine engine, string source, bool isFile = false)
+        {
+            try
+            {
+                var sw = new Stopwatch();
 
-				sw.Start();
-				var pattern = isFile
-					? RantProgram.CompileFile(source)
-					: RantProgram.CompileString(source);
-				sw.Stop();
-				var compileTime = sw.Elapsed;
+                sw.Start();
+                var pattern = isFile
+                    ? source.EndsWith(".rantpgm")
+                        ? RantProgram.LoadFile(source)
+                        : RantProgram.CompileFile(source)
+                    : RantProgram.CompileString(source);
+                sw.Stop();
 
-				sw.Restart();
-				var output = USE_SEED
-					? engine.Do(pattern, SEED, 0, PATTERN_TIMEOUT)
-					: engine.Do(pattern, 0, PATTERN_TIMEOUT);
-				sw.Stop();
+                var compileTime = sw.Elapsed;
 
-				var runTime = sw.Elapsed;
+                sw.Restart();
+                var output = USE_SEED
+                    ? engine.Do(pattern, SEED, 0, PATTERN_TIMEOUT)
+                    : engine.Do(pattern, 0, PATTERN_TIMEOUT);
+                sw.Stop();
 
-				bool writeToFile = !string.IsNullOrEmpty(Property("out"));
-				foreach (var chan in output)
-				{
-					if (chan.Name != "main")
-					{
-						if (Flag("main")) continue;
-						if (!writeToFile)
-						{
-							ForegroundColor = ConsoleColor.DarkCyan;
-							WriteLine($"{chan.Name}:");
-							ResetColor();
-						}
-					}
-					ForegroundColor = ConsoleColor.Green;
-					if (chan.Value.Length > 0)
-					{
-						if (pattern.Type == RantProgramOrigin.File && writeToFile)
-						{
-							string path = Property("out");
-							File.WriteAllText(
-								Path.Combine(Path.GetDirectoryName(path),
-									Path.GetFileNameWithoutExtension(path) +
-									(chan.Name != "main"
-										? $".{chan.Name}"
-										: "" + "." + Path.GetExtension(path))),
-								chan.Value);
-						}
-						else
-						{
+                var runTime = sw.Elapsed;
+
+                bool writeToFile = !string.IsNullOrEmpty(Property("out"));
+                foreach (var chan in output)
+                {
+                    if (chan.Name != "main")
+                    {
+                        if (Flag("main")) continue;
+                        if (!writeToFile)
+                        {
+                            ForegroundColor = ConsoleColor.DarkCyan;
+                            WriteLine($"{chan.Name}:");
+                            ResetColor();
+                        }
+                    }
+                    ForegroundColor = ConsoleColor.Green;
+                    if (chan.Value.Length > 0)
+                    {
+                        if (pattern.Type == RantProgramOrigin.File && writeToFile)
+                        {
+                            string path = Property("out");
+                            File.WriteAllText(
+                                Path.Combine(Path.GetDirectoryName(path),
+                                    Path.GetFileNameWithoutExtension(path) +
+                                    (chan.Name != "main"
+                                        ? $".{chan.Name}"
+                                        : "" + "." + Path.GetExtension(path))),
+                                chan.Value);
+                        }
+                        else
+                        {
 #if DEBUG
                             WriteLine($"'{chan.Value}'");
 #else
-							WriteLine(chan.Value.Normalize(NormalizationForm.FormC));
+                            WriteLine(chan.Value.Normalize(NormalizationForm.FormC));
 #endif
-						}
-					}
-					else if (!writeToFile)
-					{
-						ForegroundColor = ConsoleColor.DarkGray;
-						if (pattern.Type != RantProgramOrigin.File) WriteLine("[Empty]");
-					}
-					ResetColor();
-					WriteLine();
-				}
+                        }
+                    }
+                    else if (!writeToFile)
+                    {
+                        ForegroundColor = ConsoleColor.DarkGray;
+                        if (pattern.Type != RantProgramOrigin.File) WriteLine("[Empty]");
+                    }
+                    ResetColor();
+                    WriteLine();
+                }
 
-				if ((pattern.Type != RantProgramOrigin.File || Flag("wait")) && !Flag("nostats"))
-				{
-					PrintStats(
-						new Stat("Seed",
-							$"{output.Seed:X16}{(output.BaseGeneration != 0 ? ":" + output.BaseGeneration : string.Empty)}"),
-						new Stat("Compile Time", compileTime.ToString("c")),
-						new Stat("Run Time", runTime.ToString("c"))
-						);
-					WriteLine();
-				}
-			}
+                if (!Flag("nostats"))
+                {
+                    PrintStats(
+                        new Stat("Seed",
+                            $"{output.Seed:X16}{(output.BaseGeneration != 0 ? ":" + output.BaseGeneration : string.Empty)}"),
+                        new Stat(isFile && source.EndsWith(".rantpgm") ? "Load Time" : "Compile Time", compileTime.ToString("c")),
+                        new Stat("Run Time", runTime.ToString("c"))
+                        );
+                    WriteLine();
+                }
+                if (isFile && Flag("wait")) Console.ReadKey();
+            }
 #if !DEBUG
-			catch (RantRuntimeException e)
-			{
-				ForegroundColor = ConsoleColor.Red;
-				WriteLine(e);
-			}
-			catch (RantCompilerException e)
-			{
-				ForegroundColor = ConsoleColor.Yellow;
-				WriteLine(e.Message);
-			}
-			catch (Exception e)
-			{
-				WriteLine(e.ToString()); // Print the whole stack trace if it isn't a Rant error
-			}
+            catch (RantRuntimeException e)
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine(e);
+            }
+            catch (RantCompilerException e)
+            {
+                ForegroundColor = ConsoleColor.Yellow;
+                WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                WriteLine(e.ToString()); // Print the whole stack trace if it isn't a Rant error
+            }
 #endif
-			finally
-			{
-				ResetColor();
-			}
-		}
+            finally
+            {
+                ResetColor();
+            }
+        }
 
-		private static void PrintStats(params Stat[] stats)
-		{
-			int alignment = stats.Max(s => s.Name.Length);
-			string fmtString = $"{{0, {alignment}}}: ";
-			foreach (var stat in stats)
-			{
-				ForegroundColor = ConsoleColor.DarkGray;
-				Write(fmtString, stat.Name);
-				ForegroundColor = ConsoleColor.DarkMagenta;
-				WriteLine(stat.Value);
-			}
-			ResetColor();
-		}
+        private static void PrintStats(params Stat[] stats)
+        {
+            int alignment = stats.Max(s => s.Name.Length);
+            string fmtString = $"{{0, {alignment}}}: ";
+            foreach (var stat in stats)
+            {
+                ForegroundColor = ConsoleColor.DarkGray;
+                Write(fmtString, stat.Name);
+                ForegroundColor = ConsoleColor.DarkMagenta;
+                WriteLine(stat.Value);
+            }
+            ResetColor();
+        }
 
-		private class Stat
-		{
-			public readonly string Name;
-			public readonly object Value;
+        private class Stat
+        {
+            public readonly string Name;
+            public readonly object Value;
 
-			public Stat(string name, object value)
-			{
-				Name = name;
-				Value = value;
-			}
-		}
-	}
+            public Stat(string name, object value)
+            {
+                Name = name;
+                Value = value;
+            }
+        }
+    }
 }
---
+++ Rant.Tools/Commands/PackCommand.cs
@@ -0,0 +1,175 @@
+﻿#region License
+
+// https://github.com/TheBerkin/Rant
+// 
+// Copyright (c) 2017 Nicholas Fleck
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in the
+// Software without restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#endregion
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Newtonsoft.Json;
+
+using Rant.Core.IO.Bson;
+using Rant.Resources;
+using Rant.Tools.Packer;
+using Rant.Vocabulary;
+
+namespace Rant.Tools.Commands
+{
+    [CommandName("pack", Description = "Packages resources in a specified directory into a .rantpkg archive.", UsesPath = true)]
+    [CommandParam("out", false, "Output path for package.")]
+    [CommandParam("version", false, "Overrides the package version string in rantpkg.json.")]
+    [CommandFlag("no-compress", "Indicates that the package content should not be compressed.")]
+    internal sealed class PackCommand : Command
+    {
+        public PackCommand()
+        {
+            
+        }
+
+        protected override void OnRun()
+        {
+            var pkg = new RantPackage();
+            var paths = CmdLine.GetPaths();
+            bool compress = !CmdLine.Flag("no-compress");
+            int stringTableMode = int.Parse(CmdLine.Property("string-table", "1"));
+
+            if (stringTableMode < 0 || stringTableMode > 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid string table mode.");
+                Console.ResetColor();
+                return;
+            }
+
+            var modeEnum = (BsonStringTableMode)stringTableMode;
+
+            Console.WriteLine("Packing...");
+
+            string contentDir = Path.GetFullPath(paths.Length == 0 ? Environment.CurrentDirectory : paths[0]);
+
+            Pack(pkg, contentDir);
+
+            string outputPath;
+            string infoPath = Path.Combine(contentDir, "rantpkg.json");
+            if (!File.Exists(infoPath))
+                throw new FileNotFoundException("rantpkg.json missing from root directory.");
+
+            var info = JsonConvert.DeserializeObject<PackInfo>(File.ReadAllText(infoPath));
+            pkg.Title = info.Title;
+            pkg.Authors = info.Authors;
+            pkg.Version = RantPackageVersion.Parse(!string.IsNullOrWhiteSpace(CmdLine.Property("version")) ? CmdLine.Property("version") : info.Version);
+            pkg.Description = info.Description;
+            pkg.ID = info.ID;
+            pkg.Tags = info.Tags;
+
+            foreach (var dep in info.Dependencies)
+            {
+                pkg.AddDependency(dep);
+            }
+
+            // -out property overrides rantpkg.json's output path
+            if (!string.IsNullOrWhiteSpace(CmdLine.Property("out")))
+            {
+                outputPath = Path.Combine(CmdLine.Property("out"), $"{pkg.ID}-{pkg.Version}.rantpkg");
+                Directory.CreateDirectory(CmdLine.Property("out"));
+            }
+            // Otherwise, use rantpkg.json's "out" property
+            else if (!string.IsNullOrWhiteSpace(info.OutputPath))
+            {
+                outputPath = Path.Combine(contentDir, info.OutputPath, $"{pkg.ID}-{pkg.Version}.rantpkg");
+                Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
+            }
+            // If it doesn't have one, put it in the package content directory
+            else
+            {
+                outputPath = Path.Combine(Directory.GetParent(contentDir).FullName, $"{pkg.ID}-{pkg.Version}.rantpkg");
+            }
+
+            Console.WriteLine($"String table mode: {modeEnum}");
+            Console.WriteLine($"Compression: {(compress ? "yes" : "no")}");
+
+            Console.WriteLine(compress ? "Compressing and saving..." : "Saving...");
+            pkg.Save(outputPath, compress, modeEnum);
+
+            Console.WriteLine("\nPackage saved to " + outputPath.Replace('\\', '/'));
+
+            Console.ResetColor();
+        }
+
+        private static void Pack(RantPackage package, string contentPath)
+        {
+            foreach (string path in Directory.EnumerateFiles(contentPath, "*.*", SearchOption.AllDirectories)
+                .Where(p => p.EndsWith(".rant") || p.EndsWith(".rants")))
+            {
+                var pattern = RantProgram.CompileFile(path);
+                string relativePath;
+                TryGetRelativePath(contentPath, path, out relativePath, true);
+                pattern.Name = relativePath;
+                package.AddResource(pattern);
+                Console.WriteLine("+ " + pattern.Name);
+            }
+
+            foreach (string path in Directory.GetFiles(contentPath, "*.table", SearchOption.AllDirectories))
+            {
+                Console.WriteLine("+ " + path);
+                var table = RantDictionaryTable.FromStream(Path.GetFileNameWithoutExtension(path), File.Open(path, FileMode.Open));
+                package.AddResource(table);
+            }
+        }
+
+        private static bool TryGetRelativePath(string rootDir, string fullPath, out string relativePath, bool removeExtension = false)
+        {
+            relativePath = null;
+            if (string.IsNullOrWhiteSpace(rootDir) || string.IsNullOrWhiteSpace(fullPath)) return false;
+            var rootParts = Path.GetFullPath(rootDir).Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+            var fullParts = Path.GetFullPath(fullPath).Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+            if (rootParts.Length == 0 || fullParts.Length <= rootParts.Length)
+            {
+                relativePath = fullPath;
+                return true;
+            }
+            for (int i = 0; i < rootParts.Length; i++)
+            {
+                if (rootParts[i] != fullParts[i]) return false;
+            }
+            var sb = new StringBuilder();
+            int indDot;
+            for (int j = rootParts.Length; j < fullParts.Length; j++)
+            {
+                if (j > rootParts.Length) sb.Append('/');
+                if (removeExtension && j == fullParts.Length - 1 && (indDot = fullParts[j].LastIndexOf('.')) > -1)
+                {
+                    sb.Append(fullParts[j].Substring(0, indDot));
+                }
+                else
+                {
+                    sb.Append(fullParts[j]);
+                }
+            }
+            relativePath = sb.ToString();
+            return true;
+        }
+    }
+}
