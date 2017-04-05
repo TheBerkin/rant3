e21902f62fba9ffa13224a2fc565bcc63a4bd7e1
Andrew Rogers <anrogers529@gmail.com>
2017-04-04 21:08:58 -0400

    Fix indentation.

--- Rant/Core/Compiler/Syntax/RstDefineSubroutine.cs
+++ Rant/Core/Compiler/Syntax/RstDefineSubroutine.cs
@@ -31,22 +31,22 @@ using Rant.Core.ObjectModel;
 
 namespace Rant.Core.Compiler.Syntax
 {
-    [RST("dsub")]
-    internal class RstDefineSubroutine : RstSubroutineBase
-    {
-        public Dictionary<string, SubroutineParameterType> Parameters;
+	[RST("dsub")]
+	internal class RstDefineSubroutine : RstSubroutineBase
+	{
+		public Dictionary<string, SubroutineParameterType> Parameters;
 
-        public RstDefineSubroutine(LineCol location) : base(location)
-        {
+		public RstDefineSubroutine(LineCol location) : base(location)
+		{
 			// Used by serializer
 		}
-		
+
 		public override IEnumerator<RST> Run(Sandbox sb)
-        {
-			if(sb.Objects.ContainsKey(Name))
+		{
+			if (sb.Objects.ContainsKey(Name))
 			{
 				var subroutines = sb.Objects[Name].Value as List<RantObject>;
-				if(subroutines.Any(s => (s.Value as RstDefineSubroutine).Parameters.Count == Parameters.Count))
+				if (subroutines.Any(s => (s.Value as RstDefineSubroutine).Parameters.Count == Parameters.Count))
 				{
 					subroutines.RemoveAll(s => (s.Value as RstDefineSubroutine).Parameters.Count == Parameters.Count);
 				}
@@ -58,38 +58,38 @@ namespace Rant.Core.Compiler.Syntax
 				list.Add(new RantObject(this));
 				sb.Objects[Name] = new RantObject(list);
 			}
-            yield break;
-        }
+			yield break;
+		}
 
-        protected override IEnumerator<RST> Serialize(EasyWriter output)
-        {
-            var iterMain = base.Serialize(output);
-            while (iterMain.MoveNext()) yield return iterMain.Current;
-            output.Write(Parameters.Count);
-            foreach (var kv in Parameters)
-            {
-                output.Write(kv.Key);
-                output.Write((byte)kv.Value);
-            }
-        }
+		protected override IEnumerator<RST> Serialize(EasyWriter output)
+		{
+			var iterMain = base.Serialize(output);
+			while (iterMain.MoveNext()) yield return iterMain.Current;
+			output.Write(Parameters.Count);
+			foreach (var kv in Parameters)
+			{
+				output.Write(kv.Key);
+				output.Write((byte)kv.Value);
+			}
+		}
 
-        protected override IEnumerator<DeserializeRequest> Deserialize(EasyReader input)
-        {
-            var iterMain = base.Deserialize(input);
-            while (iterMain.MoveNext()) yield return iterMain.Current;
-            int pCount = input.ReadInt32();
-            if (Parameters == null) Parameters = new Dictionary<string, SubroutineParameterType>(pCount);
-            for (int i = 0; i < pCount; i++)
-            {
-                string key = input.ReadString();
-                Parameters[key] = (SubroutineParameterType)input.ReadByte();
-            }
-        }
+		protected override IEnumerator<DeserializeRequest> Deserialize(EasyReader input)
+		{
+			var iterMain = base.Deserialize(input);
+			while (iterMain.MoveNext()) yield return iterMain.Current;
+			int pCount = input.ReadInt32();
+			if (Parameters == null) Parameters = new Dictionary<string, SubroutineParameterType>(pCount);
+			for (int i = 0; i < pCount; i++)
+			{
+				string key = input.ReadString();
+				Parameters[key] = (SubroutineParameterType)input.ReadByte();
+			}
+		}
 	}
 
-    internal enum SubroutineParameterType : byte
-    {
-        Loose,
-        Greedy
-    }
+	internal enum SubroutineParameterType : byte
+	{
+		Loose,
+		Greedy
+	}
 }
